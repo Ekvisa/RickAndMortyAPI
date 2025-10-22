d34e55db11a7b824f7288b716fc173ed1c4b854f
Ekvisa <katishh@gmail.com>
2025-10-23 03:14:37 +0400

    almost done; todo: quotes and backend

--- script.js
+++ script.js
@@ -1,130 +1,199 @@
 const PATH = "https://rickandmortyapi.com/api";
 const MY_PATH = "http://localhost:3002";
-
-function getCharacters(p, filter) {
-  currentP.textContent = p;
-  let query = `page=${p}`;
-  loading = true;
-  if (filter) {
-    query = filter;
-  }
-  // Запрос к обычным персонажам на rickandmortyapi:
-  const mainFetch = fetch(`${PATH}/character/?${query}`).then((r) => r.json());
-
-  // Запрос к моим персонажам (добавим их только на первую страницу, после Рика):
-  const myFetch =
-    p === 1
-      ? fetch(`${MY_PATH}/character`).then((r) => r.json())
-      : Promise.resolve([]);
-
-  Promise.all([mainFetch, myFetch]).then(([data, mydata]) => {
-    console.log(data);
-    console.log(mydata);
-    loading = false;
-    maxPage = data.info.pages;
-    totalP.textContent = maxPage;
-    clearPage();
-    const characters = data.results;
-    const updatedCharacters = [
-      characters[0],
-      ...mydata,
-      ...characters.slice(1),
-    ];
-    updatedCharacters.forEach((element) => {
-      showPage(element);
-    });
-  });
-  //   Promise.all([
-  //     fetch(`${PATH}/character/?${query}`)
-  //       .then((response) => response.json())
-  //       .then((data) => {
-  //         console.log(data);
-  //         loading = false;
-  //         maxPage = data.info.pages;
-  //         totalP.textContent = maxPage;
-  //         clearPage();
-  //         data.results.forEach((element) => {
-  //           showPage(element);
-  //         });
-  //       }),
-  //     fetch(`${MY_PATH}/character/`)
-  //       .then((myresponse) => myresponse.json())
-  //       .then((mydata) => {
-  //         console.log(mydata);
-  //         loading = false;
-  //         mydata.forEach((element) => {
-  //           showPage(element);
-  //         });
-  //       }),
-  //   ]);
-}
-
-// function getCharacters(p, filter) {
-//   currentP.textContent = p;
-//   let query = `page=${p}`;
-//   loading = true;
-//   if (filter) {
-//     query = filter;
-//   }
-//   fetch(`${PATH}/character/?${query}`)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       console.log(data);
-//       loading = false;
-//       maxPage = data.info.pages;
-//       totalP.textContent = maxPage;
-//       clearPage();
-//       data.results.forEach((element) => {
-//         showPage(element);
-//       });
-//     });
-// }
-
-let page = 1;
-let maxPage = 0;
-let loading = false;
+let allCharacters = []; // все персонажи
+let fullArrayToShow = []; // персонажи, с которыми работаем (напр., фильтрованные)
+let loading = true;
+let currentPage = 1;
+let pagesCount = 1;
+const PER_PAGE = 20;
+const currentP = document.querySelector("#currentPage");
+const totalP = document.querySelector("#totalPages");
 const main = document.querySelector("main");
 const prevBtn = document.querySelector("#prev");
 const nextBtn = document.querySelector("#next");
-const filterBtn = document.querySelector("#filter");
-const currentP = document.querySelector("#currentPage");
-const totalP = document.querySelector("#totalPages");
+const searchInput = document.querySelector("#search");
+
+// Добрая цитата
+// const quotes = [
+//   "В каждой вселенной найдётся кто-то, кто не сдаётся.",
+//   "Мир может быть безумен, но это не повод переставать думать.",
+//   "Иногда самый разумный — тот, кого объявили сумасшедшим.",
+//   "Когда логика рушится, остаётся только совесть.",
+//   "Пока есть хотя бы один, кто помнит, что такое добро, — вселенная не потеряна.",
+// ];
+fetch("quotes.json")
+  .then((response) => response.json())
+  .then((quotes) => {
+    console.log(quotes);
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    document.querySelector("#quote").textContent = randomQuote;
+  });
+
+// Загружаем всех персонажей из Rick&MortyAPI:
+async function fetchOfficialCharacters() {
+  let officialCharacters = [];
+  let url = `${PATH}/character/`;
+  while (url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    officialCharacters = officialCharacters.concat(data.results);
+    url = data.info.next;
+  }
+  return officialCharacters;
+}
+
+// Объединяем персонажей из Rick&MortyAPI с кастомными:
+async function joinCharacters() {
+  const [mainData, myData] = await Promise.all([
+    fetchOfficialCharacters(),
+    fetch(`${MY_PATH}/character`).then((r) => r.json()),
+  ]);
+  const joined = [mainData[0], ...myData, ...mainData.slice(1)];
+
+  //   allCharactersOriginal = joined;
+  //   allCharactersCopy = [...joined];
+  //   return allCharactersCopy;
+  return joined;
+}
+
+// Получим из массива персонажей тех, которые попадут на заданную страницу:
+function getPage(array, page) {
+  const start = (page - 1) * PER_PAGE;
+  return array.slice(start, start + PER_PAGE);
+}
+
+// Переход по страницам - delete it?
+// function changePage(page) {
+//   //   currentPage = page;
+//   showPage(getPage(filteredCharacters, page));
+// }
+
+// Отобразим персонажей из заданной страницы данных на странице сайта:
+function showPage(characters, pageNumber) {
+  const charactersToShow = getPage(characters, pageNumber);
+
+  pagesCount = Math.ceil(characters.length / PER_PAGE);
+  loading = false;
+  totalP.textContent = pagesCount;
+
+  clearPage();
+  currentP.textContent = pageNumber;
+
+  if (currentPage === 1) {
+    prevBtn.disabled = true;
+    prevBtn.classList.remove("active");
+  }
+  if (currentPage === pagesCount) {
+    nextBtn.disabled = true;
+    nextBtn.classList.remove("active");
+  }
+
+  charactersToShow.forEach((el) => {
+    const card = document.createElement("div");
+    const title = document.createElement("p");
+    const img = document.createElement("img");
+    card.classList.add("card");
+    title.textContent = el.name;
+    img.src = el.image;
+    card.append(title, img);
+    main.append(card);
+    card.addEventListener("click", () => showDetails(el));
+  });
+}
+
+searchInput.addEventListener("input", filterCharacters);
+
+function filterCharacters() {
+  const value = searchInput.value.toLowerCase();
+  if (value === "") {
+    fullArrayToShow = allCharacters;
+  } else {
+    fullArrayToShow = allCharacters.filter((char) =>
+      char.name.toLowerCase().includes(value)
+    );
+  }
+  currentPage = 1;
+  showPage(fullArrayToShow, currentPage);
+}
+
+joinCharacters().then((characters) => {
+  allCharacters = characters; // сохраняем оригинал
+  fullArrayToShow = [...characters]; // копия для отображения
+  showPage(fullArrayToShow, currentPage);
+});
 
 nextBtn.addEventListener("click", () => {
-  if (loading) return;
-  if (page === maxPage) return;
-  page++;
-  getCharacters(page);
+  if (currentPage < Math.ceil(fullArrayToShow.length / PER_PAGE)) {
+    currentPage++;
+    showPage(fullArrayToShow, currentPage);
+  }
 });
 
 prevBtn.addEventListener("click", () => {
-  if (loading) return;
-  if (page === 1) return;
-  page--;
-  getCharacters(page);
+  if (currentPage > 1) {
+    currentPage--;
+    showPage(fullArrayToShow, currentPage);
+  }
 });
-
-filterBtn.addEventListener("click", () => {
-  if (loading) return;
-  getCharacters(0, "name=rick");
-});
-
-function showPage(character) {
-  const div = document.createElement("div");
-  const title = document.createElement("span");
-  const species = document.createElement("span");
-  const img = document.createElement("img");
-  div.classList.add("card");
-
-  title.textContent = character.name;
-  species.textContent = character.specises;
-  img.src = character.image;
-  div.append(title, img, species);
-  main.append(div);
-}
 
 function clearPage() {
+  nextBtn.disabled = false;
+  nextBtn.classList.add("active");
+  prevBtn.disabled = false;
+  prevBtn.classList.add("active");
   main.innerHTML = "";
 }
 
-getCharacters(page);
+function showDetails(character) {
+  const modal = document.createElement("div");
+  modal.className = "modal";
+
+  modal.innerHTML = `
+    <div class="modal-content">
+      <span class="close">&times;</span>
+      <h2>${character.name}</h2>
+      <img src="${character.image}" alt="${character.name}">
+      <p><b>Status:</b> ${character.status}</p>
+      <p><b>Species:</b> ${character.species}</p>
+      <p><b>Gender:</b> ${character.gender}</p>
+      <p><b>Origin:</b> ${character.origin.name}</p>
+      <p><b>Location:</b> ${character.location.name}</p>
+      ${
+        character.location.name.includes("S-90")
+          ? `<a href="https://youtu.be/5K0njIual6Q?si=xF0MDW6ECFP3s4DS" target="_blank">🎥 Watch the episode</a>`
+          : ""
+      }
+    </div>`;
+
+  document.body.appendChild(modal);
+  modal.querySelector(".close").onclick = () => modal.remove();
+}
+
+// 40 случайных звезд в верхней половине экрана:
+drawStars(40, 0, 50);
+// 20 случайных звезд в нижней половине экрана:
+drawStars(20, 50, 100);
+
+function drawStars(starsCount, from, to) {
+  for (let i = 0; i < starsCount; i++) {
+    const star = document.createElement("div");
+    star.className = "star";
+    const starSize = Math.random() * 10 + 1;
+    star.style.width = star.style.height = `${starSize}px`;
+    star.style.left = `${Math.random() * 100}%`;
+    star.style.top = `${Math.random() * to + from}%`;
+    document.body.appendChild(star);
+  }
+}
+
+document.addEventListener("click", (event) => {
+  if (
+    event.target !== searchInput &&
+    event.target !== nextBtn &&
+    event.target !== prevBtn &&
+    searchInput.value
+  ) {
+    searchInput.value = "";
+    filterCharacters();
+  }
+});
